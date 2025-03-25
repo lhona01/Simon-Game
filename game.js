79b8930dc27a6c1594aa0fafc5fd4cfb7b10a967
Lomus Hona <132799004+lhona01@users.noreply.github.com>
2025-03-25 15:39:39 -0500

    Add files via upload

---
+++ game.js
@@ -0,0 +1,92 @@
+var level = 1;
+var gameOver = true;
+var colors = ["green", "red", "yellow", "blue"];
+var sequence = [];
+var currSeq = 0;
+
+function main() {
+    $(document).ready(function () {
+        $(document).on("keyup", function () {
+            if (gameOver === true) {
+                $("h1").text("Level " + level);
+                gameOver = false;
+                getRandomColor();
+            }
+        })
+    
+        $("#green").click(function () {
+            onColorClicked("green", "./sounds/green.mp3");
+        })
+        
+        $("#red").click(function () {
+            onColorClicked("red", "./sounds/red.mp3");
+        })
+        
+        $("#yellow").click(function () {
+            onColorClicked("yellow", "./sounds/yellow.mp3");
+        })
+        
+        $("#blue").click(function () {
+            onColorClicked("blue", "./sounds/blue.mp3");
+        })
+    });
+}
+
+function getRandomColor() {
+    var random = Math.floor(Math.random() * 4);
+    let color = "#" + colors[random];
+    
+    sequence.push(colors[random]);
+    $(color).css("background-color", "black");
+    setTimeout(function() {
+        $(color).css("background-color", colors[random]);
+    }, 500);
+}
+
+function onColorClicked(color, audioPath) {
+    var button = "#" + color;
+    var audio = new Audio(audioPath);
+
+    $(button).addClass("pressed");
+    setTimeout(function() {
+        $(button).removeClass("pressed");
+    }, 100);
+    audio.play();
+
+    // game over
+    if (sequence[currSeq] !== color)
+        resetGame();
+    // next level
+    else if ((currSeq + 1) === sequence.length)
+        nextLevel();
+    else
+        currSeq++;
+}
+
+function nextLevel() {
+    level++;
+    currSeq = 0;
+    $("h1").text("Level " + level);
+    setTimeout(function () {
+        getRandomColor();
+    }, 500);
+}
+
+function resetGame() {
+    var audio = new Audio("./sounds/wrong.mp3");
+    audio.play();
+
+    $("body").css("background-color", "red");
+    setTimeout(function () {
+        $("body").css("background-color", "#011f3f");
+    }, 500);
+
+    $("h1").text("Game Over, Press Any Key to Restart");
+
+    level = 1;
+    currSeq = 0;
+    sequence = [];
+    gameOver = true;
+}
+
+main();
